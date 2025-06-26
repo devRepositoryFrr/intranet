cb3931ab6a62cc52dc67d5b12eef9cd0f5b16651
Juan Manuel Vargas <manolo112008@hotmail.es>
2025-06-26 13:08:03 -0600

    update mail soporte

--- src/components/modal/ModalSp.jsx
+++ src/components/modal/ModalSp.jsx
@@ -9,9 +9,9 @@ import Alert from '../../assets/js/Alerta.js';
 import Axios from 'axios';
 import { disabled_button, enabled_button } from '../../assets/js/botones'
 function obtenerTipoSoporte() {
-	
 
-	var user = (JSON.parse(localStorage.getItem('credenciales'))==null)?JSON.parse('[{"email":"notloged","nombre":""}]'):user=JSON.parse(localStorage.getItem('credenciales'));
+
+	var user = (JSON.parse(localStorage.getItem('credenciales')) == null) ? JSON.parse('[{"email":"notloged","nombre":""}]') : user = JSON.parse(localStorage.getItem('credenciales'));
 	$.each(user[0], function (key, value) {
 		$("#soporte_txt" + key).text(value);
 	});
@@ -29,7 +29,7 @@ function obtenerTipoSoporte() {
 }
 
 let subject = "soportesistemas@conavi.gob.mx";
-//let subject = "lmmendoza@conavi.gob.mx";
+//let subject = "jmvargas@conavi.gob.mx";
 
 
 export default function ModalSp() {
@@ -41,26 +41,26 @@ export default function ModalSp() {
 		$("#add-archivo").click(function () {
 			botones = $("#botones").val();
 			$("#d-sp" + botones).removeClass("hidden");
-	
+
 			if ($("#botones").val() >= 5) {
 				$("#add-archivo").addClass("disabled");
 				$("#add-archivo").prop("disabled", true);
 			}
 			botones++;
 			$("#botones").val(botones);
-	
+
 		});
-	
+
 		$("#status").hide();
 		var window_focus;
-	
+
 		$(window).focus(function () {
 			window_focus = true;
 		}).blur(function () {
 			window_focus = false;
 		});
-	
-	
+		
+
 		function ImageToBase64(input) {
 			var img = $("#f-" + input).prop('files')[0];
 			var reader = new FileReader();
@@ -69,11 +69,12 @@ export default function ModalSp() {
 			}
 			reader.readAsDataURL(img);
 		}
-	
+
 		$(".files_").bind("click", function () {
 			var id_ = this.id.toString().split("-");
-	
-	
+
+
+
 			var loopFocus = setInterval(function () {
 				if (window_focus) {
 					clearInterval(loopFocus);
@@ -92,15 +93,15 @@ export default function ModalSp() {
 				}
 			}, 500);
 		});
-	
-	
+
+
 		$('#d-sp1').click(function () { $('#f-sp1')[0].click(); });
 		$('#d-sp2').click(function () { $('#f-sp2')[0].click(); });
 		$('#d-sp3').click(function () { $('#f-sp3')[0].click(); });
 		$('#d-sp4').click(function () { $('#f-sp4')[0].click(); });
 		$('#d-sp5').click(function () { $('#f-sp5')[0].click(); });
-	
-	
+
+
 		function validar_ticket() {
 			var des, tipo;
 			let objSoporte = $("#frmSoporte").serializeToJSON();
@@ -108,27 +109,27 @@ export default function ModalSp() {
 				des = val.txtdescripcion;
 				tipo = val.cbotipo;
 			});
-	
+
 			if (des != "" && tipo != "0") {
 				enabled_button("btnGuardar-sp");
 			} else {
 				disabled_button("btnGuardar-sp");
 			}
 		}
-	
-	
+
+
 		$("#soporte_txtdescripcion").keyup(function () {
 			validar_ticket();
 		});
-	
+
 		$("#soporte_cbotipo").change(function () {
 			validar_ticket();
 		});
-	
-	
+
+
 	});
 
-	
+
 
 
 
@@ -147,8 +148,20 @@ export default function ModalSp() {
 		}
 	}
 
+	function getExtension(nombre) {
+
+		let x = nombre.split(".");
+		let i = x.length;
+		console.log("some" + i)
+		return x[i - 1];
+	}
 	const submitInsert = () => {
 		var user = JSON.parse(localStorage.getItem('credenciales'));
+		let ext1 = getExtension($("#f-sp1").val());
+		let ext2 = getExtension($("#f-sp2").val());
+		let ext3 = getExtension($("#f-sp3").val());
+		let ext4 = getExtension($("#f-sp4").val());
+		let ext5 = getExtension($("#f-sp5").val());
 		Axios.post('https://sistemaintegral.conavi.gob.mx:81/api/nuevo_ticket', {
 			usuario: user[0].email,
 			id_soporte: $("#soporte_cbotipo").children("option:selected").val(),
@@ -160,7 +173,7 @@ export default function ModalSp() {
 
 					//const imageStr = fileToBase64(document.getElementById('f-sp1').files[0]);
 
-					////console.log(b64toBlob(document.getElementById('f-sp1').files[0]));
+					console.log($("#t-sp1").val().toString().replace("data:image/jpeg;base64,", ""));
 
 					enviarEmail(
 						/*aplicativo*/"INTRANET - CONAVI:",
@@ -170,11 +183,11 @@ export default function ModalSp() {
 						"Descripción <strong>" + $("#soporte_txtdescripcion").val() + "</strong>.",
 						/*modulo*/"Nuevo ticket",
 						/*fotos*/
-						$("#t-sp1").val().toString().replace("data:image/jpeg;base64,", ""),
-						$("#t-sp2").val().toString().replace("data:image/jpeg;base64,", ""),
-						$("#t-sp3").val().toString().replace("data:image/jpeg;base64,", ""),
-						$("#t-sp4").val().toString().replace("data:image/jpeg;base64,", ""),
-						$("#t-sp5").val().toString().replace("data:image/jpeg;base64,", ""));
+						$("#t-sp1").val().toString().replace("data:image/jpeg;base64,", "").replace("data:application/pdf;base64,", ""),
+						$("#t-sp2").val().toString().replace("data:image/jpeg;base64,", "").replace("data:application/pdf;base64,", ""),
+						$("#t-sp3").val().toString().replace("data:image/jpeg;base64,", "").replace("data:application/pdf;base64,", ""),
+						$("#t-sp4").val().toString().replace("data:image/jpeg;base64,", "").replace("data:application/pdf;base64,", ""),
+						$("#t-sp5").val().toString().replace("data:image/jpeg;base64,", "").replace("data:application/pdf;base64,", ""), ext1,ext2,ext3,ext4,ext5);
 
 					limpiar();
 					Alert("¡Registraste un nuevo ticket con<strong> éxito</strong>!", "alertaOp alert alert-success alert-dismissible");
@@ -192,9 +205,9 @@ export default function ModalSp() {
 
 	};
 
-	const enviarEmail = (aplicativo, cuerpo, asunto, mensaje, modulo, img1, img2, img3, img4, img5) => {
-		Axios.post("https://sistemaintegral.conavi.gob.mx:81/api/send_mail", {
-			// Axios.post("http://localhost:3001/api/send_mail", {
+	const enviarEmail = (aplicativo, cuerpo, asunto, mensaje, modulo, img1, img2, img3, img4, img5, ext1, ext2, ext3, ext4, ext5) => {
+		Axios.post("https://sistemaintegral.conavi.gob.mx:81/api/send_mailsp", {
+	//	Axios.post("http://localhost:3001/api/send_mailsp", {
 			aplicativo,
 			cuerpo,
 			asunto,
@@ -202,6 +215,11 @@ export default function ModalSp() {
 			subject,
 			modulo,
 			img1,
+			ext1,
+			ext2,
+			ext3,
+			ext4,
+			ext5,
 			img2,
 			img3,
 			img4,
@@ -219,7 +237,7 @@ export default function ModalSp() {
 				<h4 id='titulo-modal' style={{ fontWeight: 300 }}>Levantamiento de tickets</h4>
 			</div>
 
-			<div class='modal-body' style={{ height: 450, overflowX: "auto", overflowY: "auto" , padding: 20 }}>
+			<div class='modal-body' style={{ height: 450, overflowX: "auto", overflowY: "auto", padding: 20 }}>
 				<form id="frmSoporte">
 
 					<p>Área: <strong><span name="soporte.txtarea" id="soporte_txtarea" retrieve="soporte_txtarea"></span></strong></p>
@@ -229,7 +247,7 @@ export default function ModalSp() {
 					</select>
 					<p>Descripción del problema:</p>
 					<div>
-						<label style={{ fontSize: 12 }}>Carateres restantes: <span class="crt-count" style={{ fontSize: 12 }}>500</span></label>
+						<label style={{ fontSize: 12 }}>Caracteres restantes: <span class="crt-count" style={{ fontSize: 12 }}>500</span></label>
 						<textarea maxLength="500"
 							name="soporte.txtdescripcion"
 							id="soporte_txtdescripcion"
@@ -261,32 +279,32 @@ export default function ModalSp() {
 				<div>
 					<div id="d-sp1" class="file-select hidden">
 						<i id="i-sp1" class="fas fa-file-upload"></i><span id="s-sp1"> Cargar un archivo</span>
-						<input class="files_" accept="image/jpg" id="f-sp1" type="file" style={{ display: "none" }}></input>
+						<input class="files_" accept="application/pdf, image/jpeg, image/png" id="f-sp1" type="file" style={{ display: "none" }}></input>
 
 					</div>
 					<input id="t-sp1" type="hidden"></input>
 
 					<div id="d-sp2" class="file-select hidden">
 						<i id="i-sp2" class="fas fa-file-upload"></i><span id="s-sp2"> Cargar un archivo</span>
-						<input class="files_" accept="image/jpg" id="f-sp2" type="file" style={{ display: "none" }}></input>
+						<input class="files_" accept="application/pdf, image/jpeg, image/png" id="f-sp2" type="file" style={{ display: "none" }}></input>
 
 					</div>
 					<input id="t-sp2" type="hidden"></input>
 					<div id="d-sp3" class="file-select hidden">
 						<i id="i-sp3" class="fas fa-file-upload"></i> <span id="s-sp3">Cargar un archivo</span>
-						<input class="files_" accept="image/jpg" id="f-sp3" type="file" style={{ display: "none" }}></input>
+						<input class="files_" accept="application/pdf,image/jpeg, image/png" id="f-sp3" type="file" style={{ display: "none" }}></input>
 
 					</div>
 					<input id="t-sp3" type="hidden"></input>
 					<div id="d-sp4" class="file-select hidden">
 						<i id="i-sp4" class="fas fa-file-upload"></i><span id="s-sp4"> Cargar un archivo</span>
-						<input class="files_" accept="image/jpg" id="f-sp4" type="file" style={{ display: "none" }}></input>
+						<input class="files_" accept="application/pdf,image/jpeg, image/png" id="f-sp4" type="file" style={{ display: "none" }}></input>
 
 					</div>
 					<input id="t-sp4" type="hidden"></input>
 					<div id="d-sp5" class="file-select hidden">
 						<i id="i-sp5" class="fas fa-file-upload"></i><span id="s-sp5"> Cargar un archivo</span>
-						<input class="files_" accept="image/jpg" id="f-sp5" type="file" style={{ display: "none" }}></input>
+						<input class="files_" accept="application/pdf,image/jpeg, image/png  " id="f-sp5" type="file" style={{ display: "none" }}></input>
 
 					</div>
 					<input id="t-sp5" type="hidden"></input>
